fbf6acc5102c25ca59574a095a964aa8b8d2c095
rajatuniques <176837105+rajatuniques@users.noreply.github.com>
2025-01-09 19:15:04 +0530

    Create operate() function
    This function this 3 arguments:- operator, a and b. a being first operand and b being the second.
    
    Inside if else block, respective function will be called along with passing 2 operand in it.

--- script.js
+++ script.js
@@ -14,6 +14,24 @@ function divide(a, b) {
     return a / b;
 }
 
+function operate(operator, a, b) {
+    if(operator==='+') {
+        add(a, b);
+    }
+    else if(operator==='-') {
+        subtract(a, b);
+    }
+    else if(operator==='*') {
+        multiply(a, b);
+    }
+    else if(operator==='/') {
+        divide(a, b);
+    }
+    else {
+        alert("Not a valid operator for this calculator!");
+    }
+}
+
 let first_operand;
 let operator;
 let second_operand;
